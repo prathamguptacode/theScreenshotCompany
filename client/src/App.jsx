363ffa6928f2ba6a4a2802385e8f7bd40422c3e7
Pratham Gupta <prathamgupta.wk@gmail.com>
2026-01-09 00:16:26 +0530

    deploying good

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import Home from './pages/home';
+import Home from './pages/Home.jsx'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import File from './pages/File';
 import { UserContext } from './context/userContext';
